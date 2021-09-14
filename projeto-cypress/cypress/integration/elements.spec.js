4b8eb71285902d456ac8576461659fc0bf64c153
Maic Miller <maicmiller42@gmail.com>
2021-09-14 18:53:07 -0300

    test():add link testing.

--- projeto-cypress/cypress/integration/elements.spec.js
+++ projeto-cypress/cypress/integration/elements.spec.js
@@ -14,4 +14,15 @@ describe('Work with basic elements', () => {
     cy.get('.facilAchar').should('contain', 'Cuidado');
     cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...');
   });
+
+  it.only('Links', () => {
+    cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+    cy.get('[href="#"]').click();
+    cy.get('#resultado').should('have.text', 'Voltou!');
+
+    cy.reload();
+    cy.get('#resultado').should('not.have.text', 'Voltou!');
+    cy.contains('Voltar').click();
+    cy.get('#resultado').should('have.text', 'Voltou!');
+  });
 });

48d15f56fe3f5c75cc2a78fadeb46d1b01852cde
Maic Miller <maicmiller42@gmail.com>
2021-09-14 20:53:59 -0300

    test():add sync wait tests.

---
+++ projeto-cypress/cypress/integration/sync.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/// <reference types="cypress"/>
+
+describe('Esperas...', () => {
+  before(() => {
+    cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+  });
+
+  beforeEach(() => {
+    cy.reload();
+  });
+
+  it('Deve aguardar o elemento estar disponível', () => {
+    cy.get('#novoCampo').should('not.exist');
+    cy.get('#buttonDelay').click();
+    cy.get('#novoCampo').should('not.exist');
+    cy.get('#novoCampo').should('exist');
+    cy.get('#novoCampo').type('Funciona!!!');
+  });
+});

7157e013b632f0f051f3543a7bc9c02470568ff1
Maic Miller <maicmiller42@gmail.com>
2021-09-14 19:24:41 -0300

    test():add Hooks.

--- projeto-cypress/cypress/integration/elements.spec.js
+++ projeto-cypress/cypress/integration/elements.spec.js
@@ -5,18 +5,28 @@
 /**
  * Verificar se um determinado texto existe na página de uma forma bem genérica.
  * Utilizando Jquery selector...
+ *
+ * Retirar comportamentos repetidos utilizando Hooks.
+ *   - before() - executa uma unica vez antes do primeiro teste.
+ *   - beforeEach() - executa antes de cada um de todos os testes.
 */
 describe('Work with basic elements', () => {
-  it('Text', () => {
+  before(() => {
     cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+  });
+
+  beforeEach(() => {
+    cy.reload();
+  });
+
+  it('Text', () => {
     cy.get('body').should('contain', 'Cuidado');
     cy.get('span').should('contain', 'Cuidado');
     cy.get('.facilAchar').should('contain', 'Cuidado');
     cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...');
   });
 
-  it.only('Links', () => {
-    cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+  it('Links', () => {
     cy.get('[href="#"]').click();
     cy.get('#resultado').should('have.text', 'Voltou!');
 

40406b99e8d1ab9c72e5bb5e250340fce5316af3
Maic Miller <maicmiller42@gmail.com>
2021-09-29 15:22:58 -0300

    test():add Validando mensagem test.

--- projeto-cypress/cypress/integration/alert.spec.js
+++ projeto-cypress/cypress/integration/alert.spec.js
@@ -65,4 +65,24 @@ describe('Work with basic elements', () => {
     });
     cy.get('#prompt').click();
   });
+
+  it.only('Validando msg', () => {
+    const stub = cy.stub().as('alerta');
+    cy.on('window:alert', stub);
+    cy.get('#formCadastrar').click()
+      .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+    cy.get('#formNome').type('Wagner');
+    cy.get('#formCadastrar').click()
+      .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+    cy.get('[data-cy=dataSobrenome]').type('Aquino');
+    cy.get('#formCadastrar').click()
+      .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+    cy.get('#formSexoMasc').click();
+    cy.get('#formCadastrar').click();
+
+    cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!');
+  });
 });
